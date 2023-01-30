ba8611a2997c9afb41782ff9d4f69ec7d3c4daa8
Mithilesh Hinge <mithhinge@gmail.com>
2023-01-30 18:37:38 +0530

    DashboardCard01/02/03 cleaned up Combined 3 cards into single component to remove redundancy Abstracted dummy chart datas into separate files under src/charts/data Removed: 1. Responsive classes 2. Image alt attributes 3. DashboardCard02 and DashboardCard03 components

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,8 +5,12 @@ import DashboardAvatars from '../partials/dashboard/DashboardAvatars';
 import FilterButton from '../partials/actions/FilterButton';
 import Datepicker from '../partials/actions/Datepicker';
 import DashboardCard01 from '../partials/dashboard/DashboardCard01';
-import DashboardCard02 from '../partials/dashboard/DashboardCard02';
-import DashboardCard03 from '../partials/dashboard/DashboardCard03';
+import Icon01 from '../images/icon-01.svg';
+import Icon02 from '../images/icon-02.svg';
+import Icon03 from '../images/icon-03.svg';
+import chartData01 from '../charts/data/ChartData01';
+import chartData02 from '../charts/data/ChartData02';
+import chartData03 from '../charts/data/ChartData03';
 import DashboardCard04 from '../partials/dashboard/DashboardCard04';
 import DashboardCard05 from '../partials/dashboard/DashboardCard05';
 import DashboardCard06 from '../partials/dashboard/DashboardCard06';
@@ -65,11 +69,11 @@ function Dashboard() {
             <div className="grid grid-cols-12 gap-6">
 
               {/* Line chart (Acme Plus) */}
-              <DashboardCard01 />
+              <DashboardCard01 icon={Icon01} title="Acme Plus" value="$24,780" badgeText="+49%" badgeClass="bg-green-500" chartData={chartData01} />
               {/* Line chart (Acme Advanced) */}
-              <DashboardCard02 />
+              <DashboardCard01 icon={Icon02} title="Acme Advanced" value="$17,489" badgeText="-14%" badgeClass="bg-yellow-500" chartData={chartData02} />
               {/* Line chart (Acme Professional) */}
-              <DashboardCard03 />
+              <DashboardCard01 icon={Icon03} title="Acme Professional" value="$9,962" badgeText="+29%" badgeClass="bg-green-500" chartData={chartData03} />
               {/* Bar chart (Direct vs Indirect) */}
               <DashboardCard04 />
               {/* Line chart (Real Time Value) */}
