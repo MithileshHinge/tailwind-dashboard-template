e26055add2c66be46c8848c3dba44bae54b94908
Mithilesh Hinge <mithhinge@gmail.com>
2023-01-31 16:50:34 +0530

    DashboardCard08/09 cleaned up Dummy table datas moved to separated files Removed responsive classes Reused BadgePill component

--- src/charts/LineChart02.jsx
+++ src/charts/LineChart02.jsx
@@ -4,6 +4,7 @@ import {
   Chart, LineController, LineElement, Filler, PointElement, LinearScale, TimeScale, Tooltip,
 } from 'chart.js';
 import 'chartjs-adapter-moment';
+import BadgePill from '../partials/BadgePill';
 
 // Import utilities
 import { tailwindConfig, formatValue } from '../utils/Utils';
@@ -135,7 +136,7 @@ function LineChart02({
         <div className="flex flex-wrap justify-between items-end">
           <div className="flex items-start">
             <div className="text-3xl font-bold text-slate-800 mr-2">$1,482</div>
-            <div className="text-sm font-semibold text-white px-1.5 bg-yellow-500 rounded-full">-22%</div>
+            <BadgePill badgeClass="bg-yellow-500">-22%</BadgePill>
           </div>
           <div className="grow ml-2 mb-1">
             <ul ref={legend} className="flex flex-wrap justify-end"></ul>
--- src/partials/dashboard/DashboardCard09.jsx
+++ src/partials/dashboard/DashboardCard09.jsx
@@ -1,45 +1,12 @@
 import React from 'react';
 import Info from '../../utils/Info';
 import BarChart from '../../charts/BarChart02';
-
-// Import utilities
-import { tailwindConfig } from '../../utils/Utils';
+import chartData09 from '../../data/ChartData09';
+import BadgePill from '../BadgePill';
 
 function DashboardCard09() {
-
-  const chartData = {
-    labels: [
-      '12-01-2020', '01-01-2021', '02-01-2021',
-      '03-01-2021', '04-01-2021', '05-01-2021',
-    ],
-    datasets: [
-      // Light blue bars
-      {
-        label: 'Stack 1',
-        data: [
-          6200, 9200, 6600, 8800, 5200, 9200,
-        ],
-        backgroundColor: tailwindConfig().theme.colors.indigo[500],
-        hoverBackgroundColor: tailwindConfig().theme.colors.indigo[600],
-        barPercentage: 0.66,
-        categoryPercentage: 0.66,
-      },
-      // Blue bars
-      {
-        label: 'Stack 2',
-        data: [
-          -4000, -2600, -5350, -4000, -7500, -2000,
-        ],
-        backgroundColor: tailwindConfig().theme.colors.indigo[200],
-        hoverBackgroundColor: tailwindConfig().theme.colors.indigo[300],
-        barPercentage: 0.66,
-        categoryPercentage: 0.66,
-      },
-    ],
-  };
-
   return (
-    <div className="flex flex-col col-span-full sm:col-span-6 bg-white shadow-lg rounded-sm border border-slate-200">
+    <div className="flex flex-col col-span-6 bg-white shadow-lg rounded-sm border border-slate-200">
       <header className="px-5 py-4 border-b border-slate-100 flex items-center">
         <h2 className="font-semibold text-slate-800">Sales VS Refunds</h2>
         <Info className="ml-2" containerClassName="min-w-80">
@@ -49,13 +16,13 @@ function DashboardCard09() {
       <div className="px-5 py-3">
         <div className="flex items-start">
           <div className="text-3xl font-bold text-slate-800 mr-2">+$6,796</div>
-          <div className="text-sm font-semibold text-white px-1.5 bg-yellow-500 rounded-full">-34%</div>
+          <BadgePill badgeClass="bg-yellow-500">-34%</BadgePill>
         </div>
       </div>
       {/* Chart built with Chart.js 3 */}
       <div className="grow">
         {/* Change the height attribute to adjust the chart height */}
-        <BarChart data={chartData} width={595} height={248} />
+        <BarChart data={chartData09} width={595} height={248} />
       </div>
     </div>
   );
