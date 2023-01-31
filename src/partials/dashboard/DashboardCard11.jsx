765468da60f249cd003b746ffe2c42cac5c8cc77
Mithilesh Hinge <mithhinge@gmail.com>
2023-01-31 17:08:46 +0530

    DashboardCard10/11 cleaned up Dummy table/chart data moved to separate file Removed responsive classes Reused BadgePill component

--- src/partials/dashboard/DashboardCard11.jsx
+++ src/partials/dashboard/DashboardCard11.jsx
@@ -1,57 +1,9 @@
 import React from 'react';
 import BarChart from '../../charts/BarChart03';
-
-// Import utilities
-import { tailwindConfig } from '../../utils/Utils';
+import chartData11 from '../../data/ChartData11';
+import BadgePill from '../../partials/BadgePill';
 
 function DashboardCard11() {
-
-  const chartData = {
-    labels: ['Reasons'],
-    datasets: [
-      {
-        label: 'Having difficulties using the product',
-        data: [131],
-        backgroundColor: tailwindConfig().theme.colors.indigo[500],
-        hoverBackgroundColor: tailwindConfig().theme.colors.indigo[600],
-        barPercentage: 1,
-        categoryPercentage: 1,
-      },
-      {
-        label: 'Missing features I need',
-        data: [100],
-        backgroundColor: tailwindConfig().theme.colors.indigo[800],
-        hoverBackgroundColor: tailwindConfig().theme.colors.indigo[900],
-        barPercentage: 1,
-        categoryPercentage: 1,
-      },
-      {
-        label: 'Not satisfied about the quality of the product',
-        data: [81],
-        backgroundColor: tailwindConfig().theme.colors.sky[400],
-        hoverBackgroundColor: tailwindConfig().theme.colors.sky[500],
-        barPercentage: 1,
-        categoryPercentage: 1,
-      },
-      {
-        label: 'The product doesn’t look as advertised',
-        data: [65],
-        backgroundColor: tailwindConfig().theme.colors.green[400],
-        hoverBackgroundColor: tailwindConfig().theme.colors.green[500],
-        barPercentage: 1,
-        categoryPercentage: 1,
-      },
-      {
-        label: 'Other',
-        data: [72],
-        backgroundColor: tailwindConfig().theme.colors.slate[200],
-        hoverBackgroundColor: tailwindConfig().theme.colors.slate[300],
-        barPercentage: 1,
-        categoryPercentage: 1,
-      },
-    ],
-  };
-
   return (
     <div className="col-span-full xl:col-span-6 bg-white shadow-lg rounded-sm border border-slate-200">
       <header className="px-5 py-4 border-b border-slate-100">
@@ -60,13 +12,13 @@ function DashboardCard11() {
       <div className="px-5 py-3">
         <div className="flex items-start">
           <div className="text-3xl font-bold text-slate-800 mr-2">449</div>
-          <div className="text-sm font-semibold text-white px-1.5 bg-yellow-500 rounded-full">-22%</div>
+          <BadgePill badgeClass="bg-yellow-500">-22%</BadgePill>
         </div>
       </div>
       {/* Chart built with Chart.js 3 */}
       <div className="grow">
         {/* Change the height attribute to adjust the chart height */}
-        <BarChart data={chartData} width={595} height={48} />
+        <BarChart data={chartData11} width={595} height={48} />
       </div>
     </div>
   );
