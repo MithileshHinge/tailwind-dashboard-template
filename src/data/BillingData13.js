ce922a621c6c578dace846e5a29a82b99eb3351f
Mithilesh Hinge <mithhinge@gmail.com>
2023-01-31 18:42:44 +0530

    DashboardCard13 cleaned up List created and populated dynamically Missing Amount header added Removed responsive classes Removed hover state

---
+++ src/data/BillingData13.js
@@ -0,0 +1,38 @@
+const billingData13 = [{
+  timestamp: 'Today',
+  bills: [
+    {
+      type: 'expense',
+      name: 'Qonto',
+      description: 'billing',
+      amount: '-$49.88',
+    }, {
+      type: 'income',
+      name: 'App.com',
+      description: 'Market Ltd 70 Wilson St London',
+      amount: '+249.88',
+    }, {
+      type: 'income',
+      name: 'Notion Labs Inc',
+      description: '',
+      amount: '+99.99',
+    }, {
+      type: 'income',
+      name: 'Market Cap Ltd',
+      description: '',
+      amount: '+1,200.88',
+    }, {
+      type: 'cancelled',
+      name: 'App.com',
+      description: 'Market Ltd 70 Wilson St London',
+      amount: '+$99.99',
+    }, {
+      type: 'expense',
+      name: 'App.com',
+      description: 'Market Ltd 70 Wilson St London',
+      amount: '-$49.88',
+    }
+  ]
+}];
+
+export default billingData13;
