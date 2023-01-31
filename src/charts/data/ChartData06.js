536b7e48ea85301cc3b0243ea113ef25aee337a5
Mithilesh Hinge <mithhinge@gmail.com>
2023-01-31 16:03:44 +0530

    DashboardCard06/07 cleaned up Table populated dynamically from data in loop Dummy table data and chart data moved to separate files Removed responsive classes

---
+++ src/charts/data/ChartData06.js
@@ -0,0 +1,27 @@
+// Import utilities
+import { tailwindConfig } from '../../utils/Utils';
+
+const chartData06 = {
+  labels: ['United States', 'Italy', 'Other'],
+  datasets: [
+    {
+      label: 'Top Countries',
+      data: [
+        35, 30, 35,
+      ],
+      backgroundColor: [
+        tailwindConfig().theme.colors.indigo[500],
+        tailwindConfig().theme.colors.blue[400],
+        tailwindConfig().theme.colors.indigo[800],
+      ],
+      hoverBackgroundColor: [
+        tailwindConfig().theme.colors.indigo[600],
+        tailwindConfig().theme.colors.blue[500],
+        tailwindConfig().theme.colors.indigo[900],
+      ],
+      hoverBorderColor: tailwindConfig().theme.colors.white,
+    },
+  ],
+};
+
+export default chartData06;
---
+++ src/data/Card07Data.js
@@ -0,0 +1,51 @@
+const card07Data = {
+  headers: ['Source', 'Visitors', 'Revenues', 'Sales', 'Conversion'],
+  body: [{
+    source: {
+      logo: '/src/images/channel-logo-01.svg',
+      name: 'Github.com',
+    },
+    visitors: '2.4K',
+    revenues: '$3,877',
+    sales: '267',
+    conversion: '4.7%',
+  }, {
+    source: {
+      logo: '/src/images/channel-logo-02.svg',
+      name: 'Twitter',
+    },
+    visitors: '2.2K',
+    revenues: '$3,426',
+    sales: '249',
+    conversion: '4.4%',
+  }, {
+    source: {
+      logo: '/src/images/channel-logo-03.svg',
+      name: 'Google (organic)',
+    },
+    visitors: '2.0K',
+    revenues: '$2,444',
+    sales: '224',
+    conversion: '4.2%',
+  }, {
+    source: {
+      logo: '/src/images/channel-logo-04.svg',
+      name: 'Vimeo.com',
+    },
+    visitors: '1.9K',
+    revenues: '$2,236',
+    sales: '220',
+    conversion: '4.2%',
+  }, {
+    source: {
+      logo: '/src/images/channel-logo-05.svg',
+      name: 'Indiehackers.com',
+    },
+    visitors: '1.7K',
+    revenues: '$2,034',
+    sales: '204',
+    conversion: '3.9%',
+  }]
+}
+
+export default card07Data;
