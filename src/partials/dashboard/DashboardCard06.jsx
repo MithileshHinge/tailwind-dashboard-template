b41f522dd3a10908fa9996641d90673105358e14
Mithilesh Hinge <mithhinge@gmail.com>
2023-02-03 13:07:00 +0530

    DashboardCard06/07 cleanup 2 Replaced chart with svg. Static chart legends. Static table.

--- src/partials/dashboard/DashboardCard06.jsx
+++ src/partials/dashboard/DashboardCard06.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
-import DoughnutChart from '../../charts/DoughnutChart';
-import chartData06 from '../../data/ChartData06';
-import DashboardCardGeneric from './DashboardCardGeneric';
+import chart from '../../images/charts/chart06.svg';
 
 function DashboardCard06() {
   return (
-    <DashboardCardGeneric cardClassName="flex flex-col col-span-4" title="Top Countries">
-      {/* Chart built with Chart.js 3 */}
-      {/* Change the height attribute to adjust the chart height */}
-      <DoughnutChart data={chartData06} width={389} height={260} />
-    </DashboardCardGeneric>
+    <div className="flex flex-col col-span-4 bg-white shadow-lg rounded-sm border border-slate-200">
+      <header className="px-5 py-4 border-b border-slate-100">
+        <h2 className="font-semibold text-slate-800">Top Countries</h2>
+      </header>
+      <div className="grow flex flex-col justify-center items-center">
+        <div className="p-6">
+          <img src={chart} />
+        </div>
+        <div className="px-5 pt-2 pb-6">
+          <ul className="flex flex-wrap justify-center -m-1">
+            <li className="mr-1">
+              <div className="btn-xs bg-white border-1 border-slate-200 text-slate-500 shadow-md">
+                <div className="w-2 h-2 bg-indigo-500 rounded-sm mr-1" />
+                <span className="flex items-center">United States</span>
+              </div>
+            </li>
+            <li className="mr-1">
+              <div className="btn-xs bg-white border-1 border-slate-200 text-slate-500 shadow-md">
+                <div className="w-2 h-2 bg-sky-400 rounded-sm mr-1" />
+                <span className="flex items-center">Italy</span>
+              </div>
+            </li>
+            <li className="mr-1">
+              <div className="btn-xs bg-white border-1 border-slate-200 text-slate-500 shadow-md">
+                <div className="w-2 h-2 bg-indigo-800 rounded-sm mr-1" />
+                <span className="flex items-center">Other</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
 
