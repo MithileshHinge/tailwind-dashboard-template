ff7217c534ae703420303805fe3ade2fdf309aad
Mithilesh Hinge <mithhinge@gmail.com>
2023-02-03 15:23:17 +0530

    Info button added in Card09 header

--- src/partials/dashboard/DashboardCard09.jsx
+++ src/partials/dashboard/DashboardCard09.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import chart from '../../images/charts/chart09.svg';
+import Info from '../../utils/Info';
 
 function DashboardCard09() {
   return (
     <div className="flex flex-col col-span-6 bg-white shadow-lg rounded-sm border border-slate-200">
-      <header className="px-5 py-4 border-b border-slate-100">
+      <header className="px-5 py-4 border-b border-slate-100 flex items-center">
         <h2 className="font-semibold text-slate-800">Sales VS Refunds</h2>
+        <Info className="ml-2" containerClassName="min-w-80">
+          <div className="text-sm">Sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</div>
+        </Info>
       </header>
       <div className="px-5 py-3">
         <div className="flex items-start">
