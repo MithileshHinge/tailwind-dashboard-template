436cbe47cd1b9233d75a6b02760b6a378def6cd3
Mithilesh Hinge <mithhinge@gmail.com>
2023-02-03 14:29:31 +0530

    DashboardCard10/11 cleanup 2 Table made static. Chart replaced by svg. Legends/title made in html. Padding and image sizes not consistent with design fixed. DashboardCardGeneric and BadgePill not used.

--- src/partials/dashboard/DashboardCard10.jsx
+++ src/partials/dashboard/DashboardCard10.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import tableData10 from '../../data/TableData10';
-import DashboardCardGeneric from './DashboardCardGeneric';
+import Image01 from '../../images/user-36-05.jpg';
+import Image02 from '../../images/user-36-06.jpg';
+import Image03 from '../../images/user-36-07.jpg';
+import Image04 from '../../images/user-36-08.jpg';
+import Image05 from '../../images/user-36-09.jpg';
 
 function DashboardCard10() {
   return (
-    <DashboardCardGeneric cardClassName="col-span-6" title="Customers">
-      <div className="p-3">
+    <div className="col-span-6 bg-white shadow-lg rounded-sm border border-slate-200">
+      <header className="px-5 py-4 border-b border-slate-100">
+        <h2 className="font-semibold text-slate-800">Customers</h2>
+      </header>
+      <div className="px-3 pt-3 pb-2">
 
         {/* Table */}
         <div className="overflow-x-auto">
@@ -29,38 +35,106 @@ function DashboardCard10() {
             </thead>
             {/* Table body */}
             <tbody className="text-sm divide-y divide-slate-100">
-              {
-                tableData10.map(customer => {
-                  return (
-                    <tr key={customer.id}>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="flex items-center">
-                          <div className="w-10 h-10 shrink-0 mr-3">
-                            <img className="rounded-full" src={customer.image} width="40" height="40" alt={customer.name} />
-                          </div>
-                          <div className="font-medium text-slate-800">{customer.name}</div>
-                        </div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{customer.email}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left font-medium text-green-500">{customer.spent}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-lg text-center">{customer.location}</div>
-                      </td>
-                    </tr>
-                  )
-                })
-              }
+              <tr>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="flex items-center">
+                    <div className="shrink-0 mr-3">
+                      <img className="rounded-full" src={Image01} width="36" height="36"/>
+                    </div>
+                    <div className="font-medium text-slate-800">Alex Shatov</div>
+                  </div>
+                </td>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="text-left">alexshatov@gmail.com</div>
+                </td>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="text-left font-medium text-green-500">$2,890.66</div>
+                </td>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="text-lg text-center">🇺🇸</div>
+                </td>
+              </tr>
+              <tr>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="flex items-center">
+                    <div className="shrink-0 mr-3">
+                      <img className="rounded-full" src={Image02} width="36" height="36"/>
+                    </div>
+                    <div className="font-medium text-slate-800">Philip Harbach</div>
+                  </div>
+                </td>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="text-left">philip.h@gmail.com</div>
+                </td>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="text-left font-medium text-green-500">$2,767.04</div>
+                </td>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="text-lg text-center">🇩🇪</div>
+                </td>
+              </tr>
+              <tr>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="flex items-center">
+                    <div className="shrink-0 mr-3">
+                      <img className="rounded-full" src={Image03} width="36" height="36"/>
+                    </div>
+                    <div className="font-medium text-slate-800">Mirko Fisuk</div>
+                  </div>
+                </td>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="text-left">mirkofisuk@gmail.com</div>
+                </td>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="text-left font-medium text-green-500">$2,996.00</div>
+                </td>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="text-lg text-center">🇫🇷</div>
+                </td>
+              </tr>
+              <tr>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="flex items-center">
+                    <div className="shrink-0 mr-3">
+                      <img className="rounded-full" src={Image04} width="36" height="36"/>
+                    </div>
+                    <div className="font-medium text-slate-800">Olga Semklo</div>
+                  </div>
+                </td>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="text-left">olga.s@cool.design</div>
+                </td>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="text-left font-medium text-green-500">$2,890.66</div>
+                </td>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="text-lg text-center">🇮🇹</div>
+                </td>
+              </tr>
+              <tr>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="flex items-center">
+                    <div className="shrink-0 mr-3">
+                      <img className="rounded-full" src={Image05} width="36" height="36"/>
+                    </div>
+                    <div className="font-medium text-slate-800">Burak Long</div>
+                  </div>
+                </td>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="text-left">longburak@gmail.com</div>
+                </td>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="text-left font-medium text-green-500">$1,890.66</div>
+                </td>
+                <td className="p-2 whitespace-nowrap">
+                  <div className="text-lg text-center">🇬🇧</div>
+                </td>
+              </tr>
             </tbody>
           </table>
-
         </div>
-
       </div>
-    </DashboardCardGeneric>
+    </div>
   );
 }
 
--- src/partials/dashboard/DashboardCard11.jsx
+++ src/partials/dashboard/DashboardCard11.jsx
@@ -1,24 +1,65 @@
 import React from 'react';
-import BarChart from '../../charts/BarChart03';
-import chartData11 from '../../data/ChartData11';
-import BadgePill from '../../partials/BadgePill';
-import DashboardCardGeneric from './DashboardCardGeneric';
+import chart from '../../images/charts/chart11.svg';
 
 function DashboardCard11() {
   return (
-    <DashboardCardGeneric cardClassName="col-span-6" title="Reason for Refunds">
+    <div className="col-span-6 bg-white shadow-lg rounded-sm border border-slate-200">
+      <header className="px-5 py-4 border-b border-slate-100">
+        <h2 className="font-semibold text-slate-800">Reason for Refunds</h2>
+      </header>
       <div className="px-5 py-3">
         <div className="flex items-start">
           <div className="text-3xl font-bold text-slate-800 mr-2">449</div>
-          <BadgePill badgeClass="bg-yellow-500">-22%</BadgePill>
+          <div className="text-sm font-semibold text-white px-1.5 rounded-full bg-yellow-500">-22%</div>
         </div>
       </div>
       {/* Chart built with Chart.js 3 */}
       <div className="grow">
         {/* Change the height attribute to adjust the chart height */}
-        <BarChart data={chartData11} width={595} height={48} />
+        <div className="grow flex flex-col justify-center">
+          <img src={chart} className="px-5"/>
+          <div className="px-5 py-2.5">
+            <ul className="text-sm divide-y divide-slate-100">
+              <li className="flex justify-between items-center pt-2.5 pb-2.5">
+                <div className="flex items-center">
+                  <div className="w-3 h-3 rounded-sm mr-3 bg-indigo-500" />
+                  <div>Having difficulties using the product</div>
+                </div>
+                <div className="font-medium ml-3 text-indigo-500">48.7%</div>
+              </li>
+              <li className="flex justify-between items-center pt-2.5 pb-2.5">
+                <div className="flex items-center">
+                  <div className="w-3 h-3 rounded-sm mr-3 bg-indigo-800" />
+                  <div>Missing features I need</div>
+                </div>
+                <div className="font-medium ml-3 text-indigo-800">24.9%</div>
+              </li>
+              <li className="flex justify-between items-center pt-2.5 pb-2.5">
+                <div className="flex items-center">
+                  <div className="w-3 h-3 rounded-sm mr-3 bg-sky-400" />
+                  <div>Not satisfied about the quality of the product</div>
+                </div>
+                <div className="font-medium ml-3 text-sky-400">19.3%</div>
+              </li>
+              <li className="flex justify-between items-center pt-2.5 pb-2.5">
+                <div className="flex items-center">
+                  <div className="w-3 h-3 rounded-sm mr-3 bg-emerald-400" />
+                  <div>The product doesn’t look as advertised</div>
+                </div>
+                <div className="font-medium ml-3 text-emerald-400">9.6%</div>
+              </li>
+              <li className="flex justify-between items-center pt-2.5 pb-2.5">
+                <div className="flex items-center">
+                  <div className="w-3 h-3 rounded-sm mr-3 bg-slate-400" />
+                  <div>Other</div>
+                </div>
+                <div className="font-medium ml-3 text-slate-400">9.3%</div>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </DashboardCardGeneric>
+    </div>
   );
 }
 
