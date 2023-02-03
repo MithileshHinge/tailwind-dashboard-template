cdeaf7b561a0035e01484b7edde8734d0ac8f52e
Mithilesh Hinge <mithhinge@gmail.com>
2023-02-03 15:15:50 +0530

    Removed unused files

--- src/partials/BadgePill.jsx
+++
@@ -1,7 +0,0 @@
-function BadgePill({ children, badgeClass }) {
-  return (
-    <div className={`text-sm font-semibold text-white px-1.5 rounded-full ${badgeClass}`}>{children}</div>
-  );
-}
-
-export default BadgePill;
