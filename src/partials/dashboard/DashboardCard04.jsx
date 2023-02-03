8124ee1d87fd3de6dc8cb37614dcbe5a73740b3d
Mithilesh Hinge <mithhinge@gmail.com>
2023-02-03 12:40:05 +0530

    DashboardCard04/05 cleanup 2 Replaced charts by svg. DashboardCardGeneric and BadgePill not used.

--- src/partials/dashboard/DashboardCard04.jsx
+++ src/partials/dashboard/DashboardCard04.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
-import BarChart from '../../charts/BarChart01';
-import chartData04 from '../../data/ChartData04';
-import DashboardCardGeneric from './DashboardCardGeneric';
+import chart from '../../images/charts/chart04.svg';
 
 function DashboardCard04() {
   return (
-    <DashboardCardGeneric cardClassName="flex flex-col col-span-6" title="Direct VS Indirect">
-      {/* Chart built with Chart.js 3 */}
-      {/* Change the height attribute to adjust the chart height */}
-      <BarChart data={chartData04} width={595} height={248} />
-    </DashboardCardGeneric>
+    <div className="flex flex-col col-span-6 bg-white shadow-lg rounded-sm border border-slate-200">
+      <header className="px-5 py-4 border-b border-slate-100">
+        <h2 className="font-semibold text-slate-800">Direct VS Indirect</h2>
+      </header>
+      <div className="px-5 py-3">
+        <ul className="flex flex-wrap">
+          <li className="mr-4">
+            <div className="inline-flex items-center">
+              <div className="w-3 h-3 rounded-full mr-2 border-3 border-sky-400"></div>
+              <span className="flex items-center">
+                <span className="text-slate-800 text-3xl font-bold mr-2">$1.7K</span>
+                <span className="text-slate-500 text-sm">Direct</span>
+              </span>
+            </div>
+          </li>
+          <li className="mr-4">
+            <div className="inline-flex items-center">
+              <div className="w-3 h-3 rounded-full mr-2 border-3 border-indigo-500"></div>
+              <span className="flex items-center">
+                <span className="text-slate-800 text-3xl font-bold mr-2">$2.4K</span>
+                <span className="text-slate-500 text-sm">Indirect</span>
+              </span>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <img src={chart} className="px-5 pb-4"/>
+    </div>
   );
 }
 
